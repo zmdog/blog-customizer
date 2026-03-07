ea37ad5806c96db4c5a96ee8ea358007ff286e9e
ZMdog <zmdogg3@gmail.com>
2026-03-08 00:36:47 +0300

    корректировка конфига

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -13,7 +13,7 @@ module.exports = {
 		filename: production
 			? 'static/scripts/[name].[contenthash].js'
 			: 'static/scripts/[name].js', // имя нашего бандла
-		publicPath: '/',
+		publicPath: production ? './' : '/',
 	},
 	//Нужно помочь вебпаку научится работать с jsx и tsx файлами для этого используют ts loader
 	module: {
